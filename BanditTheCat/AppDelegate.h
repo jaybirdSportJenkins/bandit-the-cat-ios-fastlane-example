1340365b6a67a7dbe6cddb230d3d1f41491ae294
rokkincat <josh@rokkincat.com>
2015-02-02 20:27:20 -0600

    Initial commit

---
+++ BanditTheCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BanditTheCat
+//
+//  Created by Josh Holtz on 2/2/15.
+//  Copyright (c) 2015 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BanditTheCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BanditTheCat
+//
+//  Created by Josh Holtz on 2/2/15.
+//  Copyright (c) 2015 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
